1a56d3f54c8e5af5dc720a156a68d50e95d9686b
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2022-07-22 23:53:08 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 菅原大輝 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
