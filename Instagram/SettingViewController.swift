9eb3bff6e850a08c2e39885c5584ddbea02da67a
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2022-07-23 12:27:26 +0900

    ■ログアウト機能の実装 ・SettingViewControllerのUI実装 ・ログアウト機能追加

--- Instagram/SettingViewController.swift
+++ Instagram/SettingViewController.swift
@@ -6,24 +6,71 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class SettingViewController: UIViewController {
 
+    @IBOutlet weak var displayNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 表示名を取得してTextFieldに設定する
+        let user = Auth.auth().currentUser
+        if let user = user {
+            displayNameTextField.text = user.displayName
+        }
     }
-    */
-
+    
+    // 表示名変更ボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleChangeButton(_ sender: Any) {
+        if let displayName = displayNameTextField.text {
+            
+            // 表示名が入力されていない場合は、HUDを表示
+            if displayName.isEmpty {
+                SVProgressHUD.showError(withStatus: "表示名を入力してください")
+                // 何もしない
+                return
+            }
+            
+            // 表示名を設定
+            let user = Auth.auth().currentUser
+            if let user = user {
+                let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = displayName
+                changeRequest.commitChanges { error in
+                    if let error = error {
+                        SVProgressHUD.showError(withStatus: "表示名の変更に失敗しました")
+                        print("DEBUG_PRINT: " + error.localizedDescription)
+                        return
+                    }
+                    print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
+                    
+                    // HUDで完了を知らせる
+                    SVProgressHUD.showSuccess(withStatus: "表示名を変更しました")
+                }
+            }
+        }
+        // キーボードを閉じる
+        self.view.endEditing(true)
+    }
+    
+    // ログアウトボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleLogoutButton(_ sender: Any) {
+        // ログアウトする
+        try! Auth.auth().signOut()
+        
+        // ログイン画面を表示する
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+        self.present(loginViewController!, animated: true, completion: nil)
+        
+        // ログイン画面から戻ってきた時のためにホーム画面を選択している状況にする
+        tabBarController?.selectedIndex = 0
+    }
 }
