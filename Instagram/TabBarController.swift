3fb4268e9a206122244408cd4d59e9cda3eb7571
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2022-07-23 09:35:00 +0900

    初回作成

---
+++ Instagram/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  Instagram
+//
+//  Created by 菅原大輝 on 2022/07/23.
+// 投稿タブ押下時の処理
+
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // タブアイコンの色
+        self.tabBar.tintColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        // タブバーの背景色
+        self.tabBar.barTintColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        // UITabBarControllerDelegateプロトコルのメソッドをこのクラスで処理する。
+        self.delegate = self
+        
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController is ImageSelectViewController {
+            // ImageSelectViewControllerは、タブ切り替えではなくモーダル画面遷移する
+            let imageSelectViewController = storyboard!.instantiateViewController(withIdentifier: "ImageSelect")
+            present(imageSelectViewController, animated: true)
+            return false
+        } else {
+            // その他のViewControllerhは通常のタブ切り替えを実施
+            return true
+        }
+    }
+    
+}
